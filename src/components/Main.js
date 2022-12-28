8aa611ed7efd8e500030dbdce531ac61ff192e33
Leonardo Vardiero <leovardiero@gmail.com>
2022-12-28 13:59:06 -0300

    Feature to add new tasks and update css

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,28 +1,67 @@
 import React, { Component } from 'react';
 
+// Form:
+import { FaPlus, FaEdit, FaWindowClose } from 'react-icons/fa';
+
+// Tarefas
+// import {  FaEdit, FaWindowClose  } from 'react-icons/fa';
+
 import './Main.css';
 
 export default class Main extends Component {
   state = {
-    novaTarefa: '',
+    newTask: '',
+    tasks: [],
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const { tasks } = this.state;
+    let { newTask } = this.state;
+
+    newTask = newTask.trim();
+
+    if (tasks.indexOf(newTask) !== -1) return;
+
+    const oldTasks = [...tasks];
+
+    this.setState({
+      tasks: [...oldTasks, newTask],
+    });
   };
 
   handleChange = (e) => {
     this.setState({
-      novaTarefa: e.target.value,
+      newTask: e.target.value,
     });
   };
 
   render() {
-    const { novaTarefa } = this.state;
+    const { newTask, tasks } = this.state;
 
     return (
       <div className="main">
-        <h1> Lista de Tarefas {novaTarefa}</h1>
-        <form action="#">
-          <input onChange={this.handleChange} type="text" />
-          <button type="submit"> Enviar </button>
+        <h1> Lista de Tarefas </h1>
+
+        <form onSubmit={this.handleSubmit} action="#" className="form">
+          <input onChange={this.handleChange} type="text" value={newTask} />
+          <button type="submit">
+            <FaPlus />
+          </button>
         </form>
+
+        <ul className="tasks">
+          {tasks.map((task) => (
+            <li key={task}>
+              {task}
+              <span>
+                <FaEdit className="edit" />
+                <FaWindowClose className="delete" />
+              </span>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
